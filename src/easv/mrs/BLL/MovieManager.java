0267f495ad5b76ddd9233d328856719ab3d49f96
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2022-11-16 09:19:55 +0100

    Initial commit

---
+++ src/easv/mrs/BLL/MovieManager.java
@@ -0,0 +1,30 @@
+package easv.mrs.BLL;
+
+import easv.mrs.BE.Movie;
+import easv.mrs.BLL.util.MovieSearcher;
+import easv.mrs.DAL.IMovieDataAccess;
+import easv.mrs.DAL.MovieDAO_Mock;
+
+import java.util.List;
+
+public class MovieManager {
+
+    private MovieSearcher movieSearcher = new MovieSearcher();
+
+    private IMovieDataAccess movieDAO;
+
+    public MovieManager() {
+        movieDAO = new MovieDAO_Mock();
+    }
+
+    public List<Movie> getAllMovies() throws Exception {
+        return movieDAO.getAllMovies();
+    }
+
+    public List<Movie> searchMovies(String query) throws Exception {
+        List<Movie> allMovies = getAllMovies();
+        List<Movie> searchResult = movieSearcher.search(allMovies, query);
+        return searchResult;
+    }
+
+}

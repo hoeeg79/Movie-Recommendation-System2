be7f0d5aaf8750f77aaf9b8e7ca979f42130d4f5
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2022-11-21 12:39:39 +0100

    d

--- src/easv/mrs/DAL/db/MovieDAO_DB.java
+++ src/easv/mrs/DAL/db/MovieDAO_DB.java
@@ -4,6 +4,7 @@ import easv.mrs.BE.Movie;
 import easv.mrs.DAL.IMovieDataAccess;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -20,10 +21,10 @@ public class MovieDAO_DB implements IMovieDataAccess {
     public List<Movie> getAllMovies() throws Exception {
         List<Movie> allMovies = new ArrayList<>();
 
-        try (Connection connection = databaseConnector.getConnection()){
-            String sql = "SELECT * FROM movie;";
+        try (Connection connection = databaseConnector.getConnection();
+             Statement statement = connection.createStatement();){
+            String sql = "SELECT * FROM Movie;";
 
-            Statement statement = connection.createStatement();
             if(statement.execute(sql)){
                 ResultSet resultSet = statement.getResultSet();
                 while (resultSet.next()){
@@ -40,8 +41,28 @@ public class MovieDAO_DB implements IMovieDataAccess {
     }
 
     public Movie createMovie(String title, int year) throws Exception {
-        //TODO Do this
-        throw new UnsupportedOperationException();
+        String sql = "INSERT INTO Movie (Title,Year)VALUES (?,?);";
+        int id = 0;
+        try ( Connection conn = databaseConnector.getConnection();
+              PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
+
+            //Bind parameters
+            stmt.setString(1,title);
+            stmt.setInt(2,year);
+
+            //Run the specified SQL statement
+            stmt.executeUpdate();
+
+            //Get generated Id from DB
+            ResultSet rs = stmt.getGeneratedKeys();
+            if(rs.next()){
+                id = rs.getInt(1);
+            }
+        }
+
+        //Generate new movie object to return
+        Movie newMovie = new Movie(id,year,title);
+        return newMovie;
     }
 
     public void updateMovie(Movie movie) throws Exception {
--- src/easv/mrs/DAL/db/MyDatabaseConnector.java
+++ src/easv/mrs/DAL/db/MyDatabaseConnector.java
@@ -18,9 +18,10 @@ public class MyDatabaseConnector {
     {
         dataSource = new SQLServerDataSource();
         dataSource.setServerName("10.176.111.31");
-        dataSource.setDatabaseName("NetflixRecommendationSystem");
-        dataSource.setUser("CSe22A_40");
-        dataSource.setPassword("CSe22A_40");
+        dataSource.setDatabaseName("lsh_MRS_2022");
+        dataSource.setUser("CSe22A_21");
+        dataSource.setPassword("CSe22A_21");
+        dataSource.setTrustServerCertificate(true);
         dataSource.setPortNumber(1433);
     }
 
